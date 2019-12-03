2e6e5fda9fb05a8ee7c670f11ffafaf99ac1875f
Alex Miller <alex.miller@cognitect.com>
2019-12-03 10:03:16 -0600

    add tools.deps.alpha custom

--- collect.clj
+++ collect.clj
@@ -1,14 +1,16 @@
 (use 'autodoc-collect.collect-info)
 
 (def custom-ns {
+  "core.async"       "clojure.core.async"
   "java.jdbc"        "clojure.java.jdbc:clojure.java.jdbc.spec"
   "spec.alpha"       "clojure.spec.alpha:clojure.spec.gen.alpha:clojure.spec.test.alpha"
-  "core.async"       "clojure.core.async"
   "tools.analyzer.jvm"  "clojure.tools.analyzer.jvm:clojure.tools.analyzer.jvm.utils:clojure.tools.analyzer.passes.jvm.analyze-host-expr:clojure.tools.analyzer.passes.jvm.annotate-branch:clojure.tools.analyzer.passes.jvm.annotate-host-info:clojure.tools.analyzer.passes.jvm.annotate-loops:clojure.tools.analyzer.passes.jvm.annotate-tags:clojure.tools.analyzer.passes.jvm.box:clojure.tools.analyzer.passes.jvm.classify-invoke:clojure.tools.analyzer.passes.jvm.constant-lifter:clojure.tools.analyzer.passes.jvm.emit-form:clojure.tools.analyzer.passes.jvm.fix-case-test:clojure.tools.analyzer.passes.jvm.infer-tag:clojure.tools.analyzer.passes.jvm.validate:clojure.tools.analyzer.passes.jvm.validate-loop-locals:clojure.tools.analyzer.passes.jvm.validate-recur:clojure.tools.analyzer.passes.jvm.warn-on-reflection"
+  "tools.deps.alpha" "clojure.tools.deps.alpha:clojure.tools.deps.alpha.reader:clojure.tools.deps.alpha.extensions:clojure.tools.deps.alpha.specs:clojure.tools.deps.alpha.util.coll:clojure.tools.deps.alpha.util.dir:clojure.tools.deps.alpha.util.io:clojure.tools.deps.alpha.util.maven:clojure.tools.deps.alpha.util.session"
   })
 
 (def custom-trim {
   "tools.analyzer.jvm" "clojure.tools.analyzer."
+  "tools.deps.alpha" "clojure.tools.deps."
   })
 
 (collect-info-to-file 
