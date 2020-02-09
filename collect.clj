f3059bff7a7b1caa61a72fa18839b0e8319a562d
Alex Miller <alex.miller@cognitect.com>
2020-02-09 09:31:38 -0600

    specialize for tools.deps.graph

--- collect.clj
+++ collect.clj
@@ -8,11 +8,13 @@
   "spec-alpha2"      "clojure.spec-alpha2:clojure.spec-alpha2.gen:clojure.spec-alpha2.test"
   "tools.analyzer.jvm"  "clojure.tools.analyzer.jvm:clojure.tools.analyzer.jvm.utils:clojure.tools.analyzer.passes.jvm.analyze-host-expr:clojure.tools.analyzer.passes.jvm.annotate-branch:clojure.tools.analyzer.passes.jvm.annotate-host-info:clojure.tools.analyzer.passes.jvm.annotate-loops:clojure.tools.analyzer.passes.jvm.annotate-tags:clojure.tools.analyzer.passes.jvm.box:clojure.tools.analyzer.passes.jvm.classify-invoke:clojure.tools.analyzer.passes.jvm.constant-lifter:clojure.tools.analyzer.passes.jvm.emit-form:clojure.tools.analyzer.passes.jvm.fix-case-test:clojure.tools.analyzer.passes.jvm.infer-tag:clojure.tools.analyzer.passes.jvm.validate:clojure.tools.analyzer.passes.jvm.validate-loop-locals:clojure.tools.analyzer.passes.jvm.validate-recur:clojure.tools.analyzer.passes.jvm.warn-on-reflection"
   "tools.deps.alpha" "clojure.tools.deps.alpha:clojure.tools.deps.alpha.reader:clojure.tools.deps.alpha.extensions:clojure.tools.deps.alpha.specs:clojure.tools.deps.alpha.util.coll:clojure.tools.deps.alpha.util.dir:clojure.tools.deps.alpha.util.io:clojure.tools.deps.alpha.util.maven:clojure.tools.deps.alpha.util.session"
+  "tools.deps.graph" "clojure.tools.deps.graph"
   })
 
 (def custom-trim {
   "tools.analyzer.jvm" "clojure.tools.analyzer."
   "tools.deps.alpha" "clojure.tools.deps."
+  "tools.deps.graph" "clojure.tools.deps."
   })
 
 (collect-info-to-file 
