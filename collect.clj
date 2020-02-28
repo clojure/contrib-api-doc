9975abd16abc849be5ae484b70dedd06fb426aac
Alex Miller <alex.miller@cognitect.com>
2020-02-27 20:56:00 -0600

    customization for tools.gitlibs

--- collect.clj
+++ collect.clj
@@ -9,12 +9,14 @@
   "tools.analyzer.jvm"  "clojure.tools.analyzer.jvm:clojure.tools.analyzer.jvm.utils:clojure.tools.analyzer.passes.jvm.analyze-host-expr:clojure.tools.analyzer.passes.jvm.annotate-branch:clojure.tools.analyzer.passes.jvm.annotate-host-info:clojure.tools.analyzer.passes.jvm.annotate-loops:clojure.tools.analyzer.passes.jvm.annotate-tags:clojure.tools.analyzer.passes.jvm.box:clojure.tools.analyzer.passes.jvm.classify-invoke:clojure.tools.analyzer.passes.jvm.constant-lifter:clojure.tools.analyzer.passes.jvm.emit-form:clojure.tools.analyzer.passes.jvm.fix-case-test:clojure.tools.analyzer.passes.jvm.infer-tag:clojure.tools.analyzer.passes.jvm.validate:clojure.tools.analyzer.passes.jvm.validate-loop-locals:clojure.tools.analyzer.passes.jvm.validate-recur:clojure.tools.analyzer.passes.jvm.warn-on-reflection"
   "tools.deps.alpha" "clojure.tools.deps.alpha:clojure.tools.deps.alpha.reader:clojure.tools.deps.alpha.extensions:clojure.tools.deps.alpha.specs:clojure.tools.deps.alpha.util.coll:clojure.tools.deps.alpha.util.dir:clojure.tools.deps.alpha.util.io:clojure.tools.deps.alpha.util.maven:clojure.tools.deps.alpha.util.session"
   "tools.deps.graph" "clojure.tools.deps.graph"
+  "tools.gitlibs" "clojure.tools.gitlibs"
   })
 
 (def custom-trim {
   "tools.analyzer.jvm" "clojure.tools.analyzer."
   "tools.deps.alpha" "clojure.tools.deps."
   "tools.deps.graph" "clojure.tools.deps."
+  "tools.gitlibs" "clojure.tools.gitlibs."
   })
 
 (collect-info-to-file 
