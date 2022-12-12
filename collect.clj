dba662465536a3bfc7ce5d58ff4ebb181287c544
Alex Miller <alex.miller@cognitect.com>
2022-12-11 21:31:26 -0600

    add new deps projects

--- collect.clj
+++ collect.clj
@@ -9,6 +9,8 @@
   "spec-alpha2"      "clojure.alpha.spec:clojure.alpha.spec.gen:clojure.alpha.spec.test"
   "tools.analyzer.jvm"  "clojure.tools.analyzer.jvm:clojure.tools.analyzer.jvm.utils:clojure.tools.analyzer.passes.jvm.analyze-host-expr:clojure.tools.analyzer.passes.jvm.annotate-branch:clojure.tools.analyzer.passes.jvm.annotate-host-info:clojure.tools.analyzer.passes.jvm.annotate-loops:clojure.tools.analyzer.passes.jvm.annotate-tags:clojure.tools.analyzer.passes.jvm.box:clojure.tools.analyzer.passes.jvm.classify-invoke:clojure.tools.analyzer.passes.jvm.constant-lifter:clojure.tools.analyzer.passes.jvm.emit-form:clojure.tools.analyzer.passes.jvm.fix-case-test:clojure.tools.analyzer.passes.jvm.infer-tag:clojure.tools.analyzer.passes.jvm.validate:clojure.tools.analyzer.passes.jvm.validate-loop-locals:clojure.tools.analyzer.passes.jvm.validate-recur:clojure.tools.analyzer.passes.jvm.warn-on-reflection"
   "tools.deps.alpha" "clojure.tools.cli.api:clojure.tools.cli.help:clojure.tools.deps.alpha:clojure.tools.deps.alpha.specs"
+  "tools.deps" "clojure.tools.deps:clojure.tools.deps.specs"
+  "tools.deps.cli" "clojure.tools.deps.cli.api:clojure.tools.deps.cli.help"
   "tools.deps.graph" "clojure.tools.deps.graph"
   "tools.gitlibs" "clojure.tools.gitlibs"
   "data.int-map" "clojure.data.int-map"
